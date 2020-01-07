7b058fbef53b5d909ecc59f105793959cd4834f3
Garry <1160564525@qq.com>
2020-01-07 18:59:37 +0800

    "xiao修xiao改"
    Signed-off-by: Garry <1160564525@qq.com>

--- 前端/js/main.js
+++ 前端/js/main.js
@@ -1,10 +1,25 @@
-import {fresh} from "./action/fresh.js"
-import {load} from "./action/ls.js"
-import {data} from "./data/data.js"
-import {unuse} from "./action/click.js"
+import {
+	fresh
+} from "./action/fresh.js"
+import {
+	load
+} from "./action/ls.js"
+import {
+	data
+} from "./data/data.js"
+import {
+	unuse
+} from "./action/click.js"
 // 导入初始化函数
-if (localStorage.getItem("mys") != null){
+if (localStorage.getItem("mys") != null) {
 	load()
 }
 fresh()
 
+$("#hook").mouseenter(function() {
+	$("#side").css("visibility", "visible")
+});
+
+$("#side").mouseleave(function() {
+	$("#side").css("visibility", "hidden")
+});
